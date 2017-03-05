9efa0e784e422029d7b349324d938cab064ca7b6
zcsd <zichun92@gmail.com>
2017-03-05 21:57:44 +0800

    First Commit

---
+++ FRG/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+    //to implement PCA first
+    
+    waitKey();
+    return 0;
+}
